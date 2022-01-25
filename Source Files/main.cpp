7fa500a54998b5fb0130f77b9f6bf51f068593c1
vaezim <91099081+vaezim@users.noreply.github.com>
2022-01-25 07:32:03 -0500

    Update main.cpp

--- Source Files/main.cpp
+++ Source Files/main.cpp
@@ -2,12 +2,6 @@
 #include "Board.h"
 #include "Piece.h" // "Board.h" does not include "Piece.h" (Error C4430).
 
-void disp(vector<int> v) {
-	for (auto& x : v)
-		cout << x << ", ";
-	cout << endl;
-}
-
 int main() {
 
 	Board board;
